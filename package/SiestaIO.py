98d982e09577fd41db2eeb8b277527a2966daaac
Magnus Paulsson <magnus.paulsson@lnu.se>
2013-02-20 07:03:16 +0000

    SDOS working same way as Siesta ... xml output

--- package/SiestaIO.py
+++ package/SiestaIO.py
@@ -1040,10 +1040,9 @@ def GetPDOSfromOrbitals(dom,index=[],atom_index=[],species=[],nlist=[],llist=[],
 
 def ReadPDOSFile(filename,index=[],atom_index=[],species=[],nlist=[],llist=[],mlist=[]):
     # Reads SIESTA *.PDOS files summing up contributions from orbitals
-    # belonging to a subset specified by the keywords
-    if filename.endswith('.gz'):
-        sys.exit('Please unzip file %s (SiestaIO.ReadPDOSFile does not support zipped PDOS-files)'%filename)
-    dom = xml.parse(filename)
+    # belonging to a subset specified by the keywords    
+    file = SIO_open(filename,mode='r')
+    dom = xml.parse(file)
     nspin = GetPDOSnspin(dom)
     norb = GetPDOSnorbitals(dom)
     ev = GetPDOSenergyValues(dom)
