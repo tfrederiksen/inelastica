58b281293d3f7eabc6d59a89ce2a26b70961f7b1
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2014-07-10 14:33:24 +0000

    Simple fixes to test scripts

--- TestCalculations/TestFDF.py
+++ TestCalculations/TestFDF.py
@@ -6,27 +6,32 @@ import numpy as N
 import numpy.random as RA
 
 def main():
+    # test 1
     buf = SIO.GetBufferAtomsList('sample.onlyS','test_buf1.fdf')
     a = N.arange(1,4)
     a = N.append(a,N.arange(41,45))
-    if N.any(a-buf != 0):
-        print buf
-        print a
+    print 'buf =',buf
+    print 'a =  ',a
+    if N.any(a-buf[0] != 0):
         raise ValueError('Error on reading in buffer atoms from test_buf1.fdf')
+    else:
+        print 'Test 1 passed'
+    # test 2
     buf = SIO.GetBufferAtomsList('sample.onlyS','test_buf2.fdf')
     a = N.arange(1,4)
     a = N.append(a,N.arange(4,6))
     a = N.append(a,N.arange(42,45))
-    if N.any(a-buf != 0):
-        print buf
-        print a
+    print 'buf =',buf
+    print 'a =  ',a
+    if N.any(a-buf[0] != 0):
         raise ValueError('Error on reading in buffer atoms from test_buf2.fdf')
-
+    else:
+        print 'Test 2 passed'
+    # test 3
     try:
         buf = SIO.GetBufferAtomsList('sample.onlyS','test_buf3.fdf')
         raise ValueError('Error in algorithm. This list is non-consecutive')
     except:
-        pass # Test passed
-
+        print 'Test 3 passed'
 
 main()
