b77e09b0e411ceabaa913288f93ae43a4b7898d7
Nick Papior <nickpapior@gmail.com>
2015-10-15 15:20:11 +0000

    Updated new_dist.sh and removed dist/
    - Updated new_dist.sh code to remove it-self from
      a distribution.
    
    - Removed dist folder.
      It contained a binary for no reason.
    
      If you want to recreate the exact 1.1 version,
      simply checkout revision 42.
    
      TODO create tag for r42 as 1.1

--- new_dist.sh
+++ new_dist.sh
@@ -10,30 +10,41 @@ echo "Found revision $r"
 
 function exp_clean {
     local dir=$1 ; shift
+
+    # Clean up the temporary directory
+    rm -rf $tmp_dir
+    mkdir $tmp_dir
+    
+    # Export current revision to the directory folder
     svn export . $tmp_dir/$dir
-    rm -rf $tmp_dir/$dir/dist
+    
+    # Remove certain files from the distribution
+    for f in new_dist.sh ; do
+	rm -r $tmp_dir/$dir/$f
+    done
 }
 
 function dist_tar {
     local out=$1 ; shift
     local dir=$1 ; shift
     pushd $tmp_dir > /dev/null
-    tar cfz ../dist/$out.tar.gz $dir
+    tar cfz ../$out.tar.gz $dir
     popd > /dev/null
 }
 
 # Create a new distribution.
 # It will ONLY take the latest committed revision.
-rm -rf $tmp_dir
-mkdir $tmp_dir
-
-#exp_clean Inelastica
-#dist_tar $pack_name Inelastica
-
-#exp_clean Inelastica-DEV
-#dist_tar $pack_name-DEV Inelastica-DEV
-
 exp_clean Inelastica-$r
 dist_tar $pack_name-$r Inelastica-$r
 
+while [ $# -gt 0 ]; do
+    
+    # Create all user distributions
+    exp_clean Inelastica-$1
+    dist_tar $pack_name-$1 Inelastica-$1
+    
+    shift
+
+done
+    
 rm -rf $tmp_dir
