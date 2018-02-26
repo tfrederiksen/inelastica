094330171c9e67c69b2dcb89329140381061127d
Nick Papior <nickpapior@gmail.com>
2018-02-26 08:47:03 +0100

    enh: added info.py file, fixes #11
    This is the basic way of getting that info attached.
    It should also handle releases easily.2
    
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- Inelastica/__init__.py
+++ Inelastica/__init__.py
@@ -1,3 +1,11 @@
+# Import version string and the major, minor, micro as well
+from . import info
+from .info import git_revision as __git_revision__
+from .info import version as __version__
+from .info import major as __major__
+from .info import minor as __minor__
+from .info import micro as __micro__
+
 from BandStruct import *
 from .CommonFunctions import *
 from .EigenChannels import *
@@ -20,3 +28,4 @@ from .WriteNetCDF import *
 from .WriteXMGR import *
 
 __all__ = [s for s in dir() if not s.startswith('_')]
+__all__ += ['__{}__'.format(s) for s in ['version', 'major', 'minor', 'micro', 'git_revision']]
