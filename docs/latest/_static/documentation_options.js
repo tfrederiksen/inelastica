9f52d2b923ed80d02d3d508988293321b2c82374
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2018-03-04 22:38:02 +0100

    docs updated

--- docs/latest/_static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '1.3.0-65',
-    LANGUAGE: 'en',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};
