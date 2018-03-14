e9bf7f2f6b7f46b5c33632ab7948a4a85caf93ca
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2018-03-14 18:28:12 +0100

    maint: removed unnecessary files

--- Examples/Graphene-SiC/C-99.5-N-0.5-position2/FCrun_52-57/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/csh 
-#$ -cwd  
-#$ -V -S /bin/bash 
-#$ -N PHrun-VCA 
-#$ -o stdout 
-#$ -e stdout 
-#$ -pe smp 24  
-mpirun  /home/emi/siesta/siesta-4.1-b2/Obj_Transiesta/transiesta   < ./RUN.fdf > ./RUN.out  
--- Examples/Graphene-SiC/C-99.5-N-0.5-position2/TSrun/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/csh 
-#$ -cwd  
-#$ -V -S /bin/bash 
-#$ -N TSrun-VCA 
-#$ -o stdout 
-#$ -e stdout 
-#$ -pe x24 24  
-mpirun  /home/emi/siesta/siesta-4.1-b2/Obj_Transiesta/transiesta   < ./RUN.fdf > ./RUN.out  
