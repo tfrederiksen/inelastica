51750c899ad66a5a8e7a36fc4f2f2d8933c3b002
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2018-09-07 13:21:25 +0200

    maint: fixed Py2 print statement

--- Examples/Phonon_bands/Au_FCC/FCrun/gen.py
+++ Examples/Phonon_bands/Au_FCC/FCrun/gen.py
@@ -8,8 +8,8 @@ for zz in range(6):
     for yy in range(6):
         for xx in range(6):
             tmp = xx*a1+yy*a2+zz*a3
-            print "%f %f %f 1"%(tmp[0], tmp[1], tmp[2])
+            print("%f %f %f 1"%(tmp[0], tmp[1], tmp[2]))
 
-print a1*6
-print a2*6
-print a3*6
+print(a1*6)
+print(a2*6)
+print(a3*6)
